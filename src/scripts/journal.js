bc8353c99d19e7096bf2eba74a4982e43a467ca7
braddavistech <brad@braddavistech.com>
2018-10-05 13:39:36 -0500

    adding info titles for journal entry output. created margins to better manage text.

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -25,15 +25,15 @@ function renderJournalEntries () {
     let journalEntryBlock = document.createElement("div");
     journalEntryBlock.className="journalEntryPrint";
     let journalElementDate = document.createElement("p");
-    let journalNodeDate = document.createTextNode(journalEntries[i].journalDate);
+    let journalNodeDate = document.createTextNode("Date: " + journalEntries[i].journalDate);
     journalElementDate.appendChild(journalNodeDate);
     journalEntryBlock.appendChild(journalElementDate);
     let journalElementConcept = document.createElement("p");
-    let journalNodeConcept = document.createTextNode(journalEntries[i].journalConcept);
+    let journalNodeConcept = document.createTextNode("Concepts Covered: " + journalEntries[i].journalConcept);
     journalElementConcept.appendChild(journalNodeConcept);
     journalEntryBlock.appendChild(journalElementConcept);
     let journalElementMessage = document.createElement("p");
-    let journalNodeMessage = document.createTextNode(journalEntries[i].journalMessage);
+    let journalNodeMessage = document.createTextNode("Thoughts/Notes: " + journalEntries[i].journalMessage);
     journalElementMessage.appendChild(journalNodeMessage);
     journalEntryBlock.appendChild(journalElementMessage);
     let journalElementMood = document.createElement("p");
@@ -49,25 +49,25 @@ function dailyMood (journalNodeMoodValue, journalEntryBlock) {
   let journalNodeMood;
   
   if (journalNodeMoodValue == "1"){
-        journalNodeMood = "Optimistic";
+        journalNodeMood = "General Mood: Optimistic";
         journalEntryBlock.id="optimisticJournalEntry";
     } else if (journalNodeMoodValue ==  "2"){
-        journalNodeMood = "Happy";
+        journalNodeMood = "General Mood: Happy";
         journalEntryBlock.id="happyJournalEntry";
     } else if (journalNodeMoodValue ==  "3"){
-        journalNodeMood = "Excited";
+        journalNodeMood = "General Mood: Excited";
         journalEntryBlock.id="excitedJournalEntry";
     } else if (journalNodeMoodValue == "4"){
-        journalNodeMood = "Tired";
+        journalNodeMood = "General Mood: Tired";
         journalEntryBlock.id="tiredJournalEntry";
     } else if (journalNodeMoodValue == "5"){
-        journalNodeMood = "Anxious";
+        journalNodeMood = "General Mood: Anxious";
         journalEntryBlock.id="anxiousJournalEntry";
     } else if (journalNodeMoodValue == "6"){
-        journalNodeMood = "Stressed";
+        journalNodeMood = "General Mood: Stressed";
         journalEntryBlock.id="stressedJournalEntry";
     } else if (journalNodeMoodValue == "7"){
-        journalNodeMood = "Sad";
+        journalNodeMood = "General Mood: Sad";
         journalEntryBlock.id="sadJournalEntry";
     };
 
